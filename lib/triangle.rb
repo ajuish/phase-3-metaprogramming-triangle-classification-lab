3a71c7cf2436b1c1fed6e4295124255f78ad0dc9
Alex Ju <aju217@gmail.com>
2022-11-03 00:17:31 -0400

    commit tree 53aa741262e7bbffcb08e3e5c24d2b810bcfd65e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Triangle
   # write code here
+  attr_reader :x, :y, :z
+
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+
+  def kind
+    validate_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  def check_sides
+    [x,y,z].all?(&:positive?)
+  end
+
+  def triangle_inequality
+    x + y > z && (x-y).abs() < z
+  end
+
+  def validate_triangle
+    raise TriangleError unless check_sides && triangle_inequality
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
